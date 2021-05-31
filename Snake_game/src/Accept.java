1ca41ddfdc7fee3aecb12b9b676346ec178ffa23
Dongkun Kim <Dongkun Kim@DESKTOP-QPURESV>
2021-05-31 09:11:10 +0900

    서버소켓 accept를 메인클래스에서 분리 - 호스트 게임화면 안뜨는 문제 해결

---
+++ Snake_game/src/Accept.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Accept implements Runnable {
+	GameBoard myGame;
+	int port;
+	
+	Accept(GameBoard myGame, int port){
+		this.myGame = myGame;
+		this.port = port;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		ServerSocket serverSocket = null;
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		int userNum = 0;
+		
+		while(true) {
+			try {
+				Socket connect = serverSocket.accept();
+				MainClass.connectList.add(connect);
+				new Thread( new Client( myGame, connect ) ).start();
+				System.out.println( "new User "+Integer.toString(userNum)+" connected." );
+				userNum++;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
